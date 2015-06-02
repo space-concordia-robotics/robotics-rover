7b67c726fedc1f5f89e5c56625577b83b40ce79e
psyomn <lethaljellybean@gmail.com>
2015-06-01 22:35:15 -0400

    Fix project layout

--- roboticsrov/microcontroller_manager.py
+++ roboticsrov/microcontroller_manager.py
@@ -1,11 +1,7 @@
-#!/usr/bin/env python2.7
-
-import serial
-
-
-ser = serial.Serial(0)  # Opens first serial port
-
-print ser.name          
-ser.write("hello")
-ser.close()
-
+class MicrocontrollerManager:
+    """
+    TODO
+    Author:
+        Doomfest
+    """
+    pass
