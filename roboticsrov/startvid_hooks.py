ae112056fb82dee9184cc95c249c83c049d6ad71
msnidal <mark.snidal@gmail.com>
2015-06-25 18:21:50 -0400

    Starts stream

--- roboticsrov/startvid_hooks.py
+++ roboticsrov/startvid_hooks.py
@@ -7,7 +7,8 @@ from roboticsnet.rover_listener import RoverListener
 
 def _startVideo():
 	os.system("killall vlc")
-	print "This is the startvid hook!"
+	print "Starting stream from startvid!"
+	os.system("cvlc v4l:///dev/video0 --sout '#transcode{vcodec=mp4v,vb=800,acodec=mpga,ab=128}:standard{access=http,mux=ts,dst=:8080}'")
 
 # First you would need to define your hooks using CommandHook
 cmd_hook = CommandHook(
