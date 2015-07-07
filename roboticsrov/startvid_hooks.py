5b9b85e17fe212aa48fb5e30e44ae84f95690107
Mark Snidal <mark.snidal@gmail.com>
2015-07-06 23:54:53 -0400

    Comments and additional case
    Adds a case for if the stream is already running and someone tries to start the stream again. Added comments throughout to clarify code

--- roboticsrov/startvid_hooks.py
+++ roboticsrov/startvid_hooks.py
@@ -5,25 +5,31 @@ import roboticsnet
 from roboticsnet.command_hook import CommandHook
 from roboticsnet.rover_listener import RoverListener
 
+# Global var videoStreamProcess holds vlc Popen subprocess, isStreaming boolean maintains stream state 
 videoStreamProcess = None
 isStreaming = False
 
+# Start vlc stream
 def _startVideo():
 	global videoStreamProcess, isStreaming
-	print "Starting stream from startvid!"
-	isStreaming = True
-	videoStreamProcess = subprocess.Popen(['cvlc', 'v4l:///dev/video0', '--sout', '#transcode{vcodec=WMV2,vb=1500}:standard{access=http,mux=ts,dst=:8080}'])
+	if (isStreaming):
+		print "Stream already running."
+	else:	
+		print "Starting stream from startvid..."
+		videoStreamProcess = subprocess.Popen(['cvlc', 'v4l:///dev/video0', '--sout', '#transcode{vcodec=WMV2,vb=1500}:standard{access=http,mux=ts,dst=:8080}'])
+		isStreaming = True
 
+# Stop vlc stream
 def _stopVideo():
 	global videoStreamProcess, isStreaming
 	if (isStreaming):
-		print "Stopping stream from stopvid!"
+		print "Stopping stream from stopvid..."
 		videoStreamProcess.kill()
 		isStreaming = False
 	else:
-		print "No stream to stop!"
+		print "No stream to stop."
 
-# First you would need to define your hooks using CommandHook
+# Hook integration
 cmd_hook = CommandHook(
         startVideo=_startVideo,
         stopVideo=_stopVideo
@@ -32,5 +38,5 @@ cmd_hook = CommandHook(
 l = RoverListener(hooks=cmd_hook,default_port=8080)
 
 print roboticsnet.__appname__, " ",  roboticsnet.__version__
-print "Starting command dispatcher..."
+print "Starting stream command dispatcher..."
 l.listen()
