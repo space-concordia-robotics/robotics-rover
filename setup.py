6a73e5dae83bda30352bb914ce7f4aa1f188a62a
psyomn <lethaljellybean@gmail.com>
2015-09-25 21:53:23 -0400

    setup.py: try yet another format for deps

--- setup.py
+++ setup.py
@@ -4,11 +4,10 @@ import roboticsrov
 
 REQUIREMENTS = [
     'pyserial>=2.7',
-    'roboticsnet==0.1.0'
 ]
 
 DEPENDENCY_LINKS = [
-    'https://github.com/space-concordia-robotics/robotics-networking.git@0.1.0#egg=roboticsnet-0.1.0'
+    'https://github.com/space-concordia-robotics/robotics-networking/tarball/master#egg=roboticsnet-0.1.0'
 ]
 
 setup(
