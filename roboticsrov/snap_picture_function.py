07d7c8dbf792c014a33e464484f2d86ec3f73d33
MickeyPa <anamikapancholy@anamikas-mbp.wireless.concordia.ca>
2015-10-24 15:25:08 -0400

    adding picture Title as a parameter, initializing the camera within the function

--- roboticsrov/snap_picture_function.py
+++ roboticsrov/snap_picture_function.py
@@ -6,28 +6,32 @@ from pygame import time
 pygame.init()
 pygame.camera.init()
 
-cam = pygame.camera.Camera("/dev/video0",(640,480)) #location and resolution needs to be changed (?)
+#cam = pygame.camera.Camera("/dev/video0",(640,480)) #location and resolution needs to be changed (?)
 #cam location is default for linux (need to double check)
-cam.start()
+#cam.start()
 
-def snap_picture (NumOfPics = 1, InitialDelay = 0, Delay = 1):
+def snap_picture (NumOfPics = 1, InitialDelay = 0, Delay = 1, Title = "picture"):
     "Takes NumOfPics pictures via the webcam with a Delay between each picture.\
      Both the InitialDelay and Delay are in seconds."
+    cam = pygame.camera.Camera("/dev/video0",(640,480))
+    cam.start()
+    counter = 0
     if NumOfPics == 1:
         time.delay(InitialDelay*1000)
         image = cam.get_image()
-        #create string that holds picture name (possibly via input)
-        Title = input("What is the name of this picture? Please also add the approproate file extension:  ")
         pygame.image.save(image, Title)
     elif NumOfPics > 1:
         time.delay(InitialDelay*1000)
-        for (NumOfPics > 0):
+        while (NumOfPics > 0):
+            counter +=1
             image = cam.get_image()
+            PicName = Title + str(counter)
             #create string that holds pictures names (e.g. pic1, pic2, pic3, pic4, etc)
-            pygame.image.save(image, "picture.jpg") 
+            pygame.image.save(image, PicName)
+            NumOfPics -= 1 
             time.delay(Delay*1000)
     else:
-        break
+        pass
     return null
 
 
