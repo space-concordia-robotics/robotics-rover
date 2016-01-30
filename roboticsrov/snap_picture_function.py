a3d7609c18eeb2e42c7c250592111d75194b317c
MickeyPa <anamikapancholy@anamikas-mbp.wireless.concordia.ca>
2016-01-30 15:36:16 -0500

    trying to move images from folders to lists

--- roboticsrov/snap_picture_function.py
+++ roboticsrov/snap_picture_function.py
@@ -9,10 +9,16 @@ from PIL import Image
 pygame.init()
 pygame.camera.init()
 
+import os
+
+
 
 ##Images is a list obtained from a folder
-##filename is the final name for the panorama
-def merge (Images, fileName):
+##mergename is the final name for the panorama
+def merge (Images, mergeName):
+
+
+
 
     OpenImages = []
     for x in Images:
@@ -21,7 +27,7 @@ def merge (Images, fileName):
     origSize = OpenImages[0].size #Gets the size of the first image in OpenImages
     origwidth = imageSize[0] #Gets the width of the first image in OpenImages
     panoWidth = OpenImages.length()*origSize[0] #Creates the new width of the panorama
-    panoHeight = imgageSize[1] #Creates the new height (which should remain unchanged) of the panorama
+    panoHeight = imageSize[1] #Creates the new height (which should remain unchanged) of the panorama
 
     #Creating the new panorama image
     Panorama = Image.new('RBG', (panoWidth, panoHeight))
@@ -31,7 +37,7 @@ def merge (Images, fileName):
         Panorama.paste(OpenImages[x], (origwidth*OpenImages.index(x),0))
 
     #Saving the panorama
-    Panorama.save(fileName)
+    Panorama.save(mergeName)
     
     return None
 
@@ -42,7 +48,7 @@ def merge (Images, fileName):
 #cam location is default for linux (need to double check)
 #cam.start()
 
-def snap_picture (NumOfPics = 1, InitialDelay = 0, Delay = 1, Title = "picture"):
+def snap_picture (NumOfPics = 1, InitialDelay = 0, Delay = 1, Title = "picture", Folder = "folder"):
     "Takes NumOfPics pictures via the webcam with a Delay between each picture.\
      Both the InitialDelay and Delay are in seconds."
     cam = pygame.camera.Camera("/dev/video0",(640,480)) #resolution of camera needs to be confirmed
@@ -51,17 +57,31 @@ def snap_picture (NumOfPics = 1, InitialDelay = 0, Delay = 1, Title = "picture")
     if NumOfPics == 1:
         time.delay(InitialDelay*1000)
         image = cam.get_image()
-        PicName = "/home/" + Title + ".jpg"
-        #save image to folder
-        pygame.image.save(image, PicName) #image needs to be networked to basestation before saving
+        PicName = Title + ".jpg"
+        pygame.image.save(image, PicName)
+        source = os.getcwd() + "/" + PicName
+        #create destination folder and get its path
+        try:
+            os.makedirs(os.getcwd() + "/" + Folder)
+        except OSError as e:
+            pass
+        finally:
+            newpath = os.getcwd() + "/" + Folder
+        shutil.move(source, newfilepath)  #need to get the src path for this to work
     elif NumOfPics > 1:
         time.delay(InitialDelay*1000)
         while (NumOfPics > 0):
             counter +=1
             image = cam.get_image()
             PicName = Title + "-" + str(counter) + ".jpg"
-            #save image to folder
-            pygame.image.save(image, PicName) #image needs to be networked to basestation before saving
+            pygame.image.save(image, PicName)
+            try:
+                os.makedirs(os.getcwd() + "/" + Folder)
+            except OSError as e:
+                pass
+            finally:
+                newpath = os.getcwd() + "/" + Folder
+            shutil.move(source, newfilepath)
             NumOfPics -= 1 
             time.delay(Delay*1000)
     else:
@@ -73,10 +93,11 @@ def snap_picture (NumOfPics = 1, InitialDelay = 0, Delay = 1, Title = "picture")
 
 def panorama (Title = "pano1"):
     #Get GPS location and input that as Title parameter in snap_picture
-    snap_picture(6, 0, 1, "Panorama")
+    snap_picture(6, 0, 1, Title,"PanoFolder")
     #pull images from where they've been saved and put them in a list
     #that list is the first argument in the merge() function below
-    merge(,Title)
+    parts = []
+    merge(parts,Title)
     return None
 
 
