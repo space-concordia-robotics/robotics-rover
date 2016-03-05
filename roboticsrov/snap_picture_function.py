d9a2d6c34f72b7c6485094150f7899d4714cbed7
MickeyPa <anamikapancholy@anamikas-mbp.wireless.concordia.ca>
2016-03-05 15:05:16 -0500

    snapshot edit
    snapshot takes only one picture

--- roboticsrov/snap_picture_function.py
+++ roboticsrov/snap_picture_function.py
@@ -46,49 +46,31 @@ def merge (Images, mergeName):
 #cam location is default for linux (need to double check)
 #cam.start()
 
-def snapshot (NumOfPics = 1, InitialDelay = 0, Delay = 1, Title = "picture", Folder = "folder"):
+def snapshot (InitialDelay = 0, Delay = 1, Title = "picture", Folder = "folder"):
     "Takes NumOfPics pictures via the webcam with a Delay between each picture.\
      Both the InitialDelay and Delay are in seconds."
     cam = pygame.camera.Camera("/dev/video0",(640,480)) #resolution of camera needs to be confirmed
     cam.start()
     counter = 0
-    if NumOfPics == 1:
-        time.delay(InitialDelay*1000)
-        image = cam.get_image()
-        PicName = Title + ".jpg"
-        #check if another file with the title already exists
-            #save all titles to a file? and check from there?
-        #if it does, add a number at the end of it
-            #make recursive.
-            #if -1 exists, check if -2 exists, etc until you hit the end then tac on the next number
-        pygame.image.save(image, PicName)
-        source = os.getcwd() + "/" + PicName
-        try:
-            os.makedirs(os.getcwd() + "/" + Folder)
-        except OSError as e:
-            pass
-        finally:
-            newpath = os.getcwd() + "/" + Folder
-        shutil.move(source, newpath)
-    elif NumOfPics > 1:
-        time.delay(InitialDelay*1000)
-        while (NumOfPics > 0):
-            counter +=1
-            image = cam.get_image()
-            PicName = Title + "-" + str(counter) + ".jpg"
-            pygame.image.save(image, PicName)
-            source = os.getcwd() + "/" + PicName
-            try:
-                os.makedirs(os.getcwd() + "/" + Folder)
-            except OSError as e:
-                pass
-            finally:
-                newpath = os.getcwd() + "/" + Folder
-            shutil.move(source, newpath)
-            NumOfPics -= 1 
-            time.delay(Delay*1000)
-    else:
+
+    time.delay(InitialDelay*1000)
+    image = cam.get_image()
+    PicName = Title + ".jpg"
+    #check if another file with the title already exists
+        #save all titles to a file? and check from there?
+    #if it does, add a number at the end of it
+        #make recursive.
+        #if -1 exists, check if -2 exists, etc until you hit the end then tac on the next number
+    pygame.image.save(image, PicName)
+    source = os.getcwd() + "/" + PicName
+    try:
+        os.makedirs(os.getcwd() + "/" + Folder)
+    except OSError as e:
         pass
+    finally:
+        newpath = os.getcwd() + "/" + Folder
+    shutil.move(source, newpath)
+
     cam.stop()
     return None
 
